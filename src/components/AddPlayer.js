317572551569b78a67cac2b25e58bfa1c8c8321b
Matt Maslen <mattmaslen123@gmail.com>
2020-12-27 15:40:52 +0000

    first working version of add player to current players list, in AddPlayer component

--- src/components/AddPlayer.js
+++ src/components/AddPlayer.js
@@ -6,22 +6,52 @@ class AddPlayer extends Component {
 
 		this.state = {
 			playerName: "",
+			players: ["bob", "charlie", "luke"],
 		};
 		this.handleChange = this.handleChange.bind(this);
+		this.addPlayer = this.addPlayer.bind(this);
 	}
 	handleChange(e) {
 		this.setState({ playerName: e.currentTarget.value });
 	}
 
+	addPlayer(e) {
+		e.preventDefault();
+
+		const { players, playerName } = this.state;
+
+		this.setState({
+			players: [...this.state.players, playerName],
+			playerName: "",
+		});
+	}
+
+	/* this.setState({ players: [...this.state.players, newPlayer] });
+		const { onSubmit } = this.props;
+		onSubmit(); */
+
 	render() {
-		let { playerName } = this.state;
+		const { playerName, players } = this.state;
 		return (
 			<>
 				<h1>ENTER PLAYER NAMES:</h1>
-				<form>
+				<form onSubmit={this.addPlayer}>
 					<label>Player #? Name:</label>
-					<input type="text" onChange={this.handleChange} value={playerName} />
+					<input
+						type="text"
+						name="playerName"
+						placeholder="Enter name"
+						value={playerName}
+						onChange={this.handleChange}
+					/>
+					<button>Add</button>
 				</form>
+				<h3>Current Players:</h3>
+				<ul>
+					{players.map((player) => {
+						return <li key={player}> {player}</li>;
+					})}
+				</ul>
 			</>
 		);
 	}
