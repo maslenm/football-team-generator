10650bb24caf964b6a316a5932e56cb5e062eb03
Matt Maslen <mattmaslen123@gmail.com>
2020-12-29 15:38:35 +0000

    updated reducer to handle logic of addplayer instead of being inside component file.

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,5 +1,19 @@
-const reducer = (state, action) => {
+const addPlayerReducer = (state, action) => {
+	if (state.playerName !== "" && state.players.length < 10)
+		return {
+			...state,
+			players: [...state.players, action.payload],
+		};
 	return state;
+};
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "ADD_PLAYER":
+			return addPlayerReducer(state, action);
+		default:
+			return state;
+	}
 };
 
 export default reducer;
