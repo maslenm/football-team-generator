b7ed86f877abb2d073c972517e123768d9b42813
Matt Maslen <mattmaslen123@gmail.com>
2020-12-30 21:33:10 +0000

    edited code to map over new team array.

--- src/components/TeamsList/TeamsList.js
+++ src/components/TeamsList/TeamsList.js
@@ -3,13 +3,13 @@ const TeamsList = ({ teams }) => (
 		<h3>Teams:</h3>
 		<h3>Team 1:</h3>
 		<ul>
-			{teams.map((player, index) => {
+			{teams.team1.map((player, index) => {
 				return <li key={index}> {player.name}</li>;
 			})}
 		</ul>
 		<h3>Team 2:</h3>
 		<ul>
-			{teams.map((player, index) => {
+			{teams.team2.map((player, index) => {
 				return <li key={index}> {player.name}</li>;
 			})}
 		</ul>
