ac8d89fda8fb80abce21984e50b095347768fe1d
Matt Maslen <mattmaslen123@gmail.com>
2020-12-30 15:06:18 +0000

    added createTeamsReducer to update teams state with players state.

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -7,10 +7,16 @@ const addPlayerReducer = (state, action) => {
 	return state;
 };
 
+const createTeamsReducer = (state, action) => {
+	return { ...state, teams: [...state.players] };
+};
+
 const reducer = (state, action) => {
 	switch (action.type) {
 		case "ADD_PLAYER":
 			return addPlayerReducer(state, action);
+		case "CREATE_TEAMS":
+			return createTeamsReducer(state, action);
 		default:
 			return state;
 	}
