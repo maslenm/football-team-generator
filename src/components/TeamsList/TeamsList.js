24b6ddf1b534ef66e53cd821d437364d91e9367d
Matt Maslen <mattmaslen123@gmail.com>
2020-12-31 11:37:02 +0000

    added some notes for future me to follow, tidied a couple of lines, removed import component line not needed in PlayerList.

--- src/components/TeamsList/TeamsList.js
+++ src/components/TeamsList/TeamsList.js
@@ -3,6 +3,7 @@ const TeamsList = ({ teams }) => (
 		<h3>Teams:</h3>
 		<h3>Team 1:</h3>
 		<ul>
+			{/* maps over the team property of teams state */}
 			{teams.team1.map((player, index) => {
 				return <li key={index}> {player.name}</li>;
 			})}
