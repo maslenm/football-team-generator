da4de4e44a3ebec8522a066c8825825149ce7b8e
Matt Maslen <mattmaslen123@gmail.com>
2021-01-02 16:17:29 +0000

    added history routing to direct to /teamslist onClick.

--- src/components/AddPlayer/index.js
+++ src/components/AddPlayer/index.js
@@ -1,11 +1,15 @@
 import AddPlayer from "./AddPlayer";
 import { connect } from "react-redux";
+import history from "../../history";
 import { addPlayer, createTeams } from "../../data/actions/state";
 
 const mapDispatchToProps = (dispatch) => {
 	return {
 		onSubmit: (data) => dispatch(addPlayer(data)),
-		handleCreate: (data) => dispatch(createTeams(data)),
+		handleCreate: (data) => {
+			dispatch(createTeams(data));
+			history.push("/teamslist");
+		},
 	};
 };
 
