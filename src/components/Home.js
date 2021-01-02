b1e8dbe5d4094c98ff0f40df2645925477541d78
Matt Maslen <mattmaslen123@gmail.com>
2021-01-02 16:20:09 +0000

    added content to welcome users, and instructions on how the site works. Includes styling classNames.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,32 @@ import { Link } from "react-router-dom";
 
 const Home = () => (
 	<>
-		<h1>Selector</h1>
-		<Link to="/addplayer">
-			<button>Start</button>
-		</Link>
+		<section className="welcome">
+			<header className="welcome-header">
+				<h2>What is Auto Team?</h2>
+			</header>
+			<article className="welcome-content">
+				<p>
+					This site allows you to enter the names of 10 people and it will
+					randomly divide them into 2 teams of equal size.
+				</p>
+			</article>
+		</section>
+		<section className="instruction">
+			<header className="instruction-header">
+				<h2>Instructions:</h2>
+			</header>
+			<ul className="instruction-list">
+				<li>Enter 10 player names</li>
+				<li>Submit your entries</li>
+				<li>View Randomly generated teams</li>
+			</ul>
+		</section>
+		<section className="start">
+			<Link to="/addplayer" className="greenButton">
+				Start
+			</Link>
+		</section>
 	</>
 );
 
