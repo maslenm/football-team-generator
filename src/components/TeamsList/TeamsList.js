1d557168a19c075d0713d2d6002c5b1ff4c16de6
Matt Maslen <mattmaslen123@gmail.com>
2021-01-03 14:25:57 +0000

    added html elements and classNames for styling.

--- src/components/TeamsList/TeamsList.js
+++ src/components/TeamsList/TeamsList.js
@@ -1,19 +1,31 @@
 const TeamsList = ({ teams }) => (
 	<>
-		<h3>Teams:</h3>
-		<h3>Team 1:</h3>
-		<ul>
-			{/* maps over the team property of teams state */}
-			{teams.team1.map((player, index) => {
-				return <li key={index}> {player.name}</li>;
-			})}
-		</ul>
-		<h3>Team 2:</h3>
-		<ul>
-			{teams.team2.map((player, index) => {
-				return <li key={index}> {player.name}</li>;
-			})}
-		</ul>
+		<section className="teams-page">
+			<header className="teams-page-header">
+				<h3>Teams :</h3>
+			</header>
+		</section>
+		<section className="teams">
+			<header className="teams-header">
+				<h3>Team 1 :</h3>
+			</header>
+			<ul className="teams-list-1">
+				{/* maps over the team property of teams state */}
+				{teams.team1.map((player, index) => {
+					return <li key={index}> {player.name}</li>;
+				})}
+			</ul>
+		</section>
+		<section className="teams">
+			<header className="teams-header">
+				<h3>Team 2 :</h3>
+			</header>
+			<ul className="teams-list-2">
+				{teams.team2.map((player, index) => {
+					return <li key={index}> {player.name}</li>;
+				})}
+			</ul>
+		</section>
 	</>
 );
 
