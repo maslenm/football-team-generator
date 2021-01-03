e4a0ecb4827b7c818bd917013085991fe2fb9b93
Matt Maslen <mattmaslen123@gmail.com>
2021-01-03 14:24:21 +0000

    added classNames to style logo text.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ const NavBar = () => (
 	<>
 		<header className="nav-header">
 			<section className="title">
-				<h1>AutoTeam</h1>
-				<h2>Football Team Picker</h2>
+				<h1 className="logoH1">AutoTeam</h1>
+				<h2 className="logoH2">Football Team Picker</h2>
 			</section>
 			<nav className="nav-main">
 				<Link to="/" className="restartButton">
